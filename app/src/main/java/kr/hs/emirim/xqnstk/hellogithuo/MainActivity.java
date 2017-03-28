5ebcc2f4e901cd753c2d69b8dac8d92a74f744dd
xqnstk <xqnstk@e-mirim.hs.kr>
2017-03-28 20:54:33 +0900

    #1 버튼 누르면 폭팔"

--- app/src/main/java/kr/hs/emirim/xqnstk/hellogithuo/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/xqnstk/hellogithuo/MainActivity.java
@@ -2,12 +2,25 @@ package kr.hs.emirim.xqnstk.hellogithuo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnhello = (Button) findViewById(R.id.btnhello);
+        btnhello.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "콰가가강", Toast.LENGTH_LONG).show();
     }
 }
